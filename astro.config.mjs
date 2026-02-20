bc590db894d3e779da83b7a1e0f930a135cf7e62
Attila Bagossy <bagossyattila@outlook.com>
2026-02-20 13:44:29 +0100

    chore: testing them link cards

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,30 +2,60 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import markdoc from "@astrojs/markdoc";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://unideb-advanced-software-engineering.github.io",
   base: "/site",
   integrations: [
     starlight({
-      title: "Szoftverrendszerek tervezése",
+      title: {
+        hu: "Szoftverrendszerek tervezése",
+        en: "Advanced Software Engineering",
+      },
+      customCss: ["./src/styles/custom.css"],
+      defaultLocale: "hu",
+      locales: {
+        en: {
+          label: "English",
+        },
+        hu: {
+          label: "Magyar",
+        },
+      },
       social: [
         {
           icon: "github",
           label: "GitHub",
-          href: "https://github.com/unideb-advanced-software-engineering/site",
+          href: "https://github.com/unideb-advanced-software-engineering",
         },
       ],
       sidebar: [
         {
-          label: "Gyakorlatok",
+          label: "Általános",
+          translations: {
+            en: "General",
+          },
+          autogenerate: { directory: "general" },
+        },
+        {
+          label: "Nappali gyakorlatok",
+          translations: {
+            en: "Classes",
+          },
           autogenerate: { directory: "classes" },
         },
         {
           label: "Esettanulmányok",
+          translations: {
+            en: "Scenarios",
+          },
+          collapsed: true,
           autogenerate: { directory: "scenarios" },
         },
       ],
     }),
+    markdoc(),
   ],
 });
