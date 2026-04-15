0682d4f27ad273b2687bff3b57f3e5c1fad64288
Attila Bagossy <bagossyattila@outlook.com>
2026-04-15 15:16:49 +0200

    feat: added the system context diagram for chocomarket

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,61 +1,64 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import { LikeC4VitePlugin } from "likec4/vite-plugin";
 
 import markdoc from "@astrojs/markdoc";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://unideb-advanced-software-engineering.github.io",
   base: "/site",
-  integrations: [
-    starlight({
-      title: {
-        hu: "Szoftverrendszerek tervezése",
-        en: "Advanced Software Engineering",
+  vite: {
+    plugins: [LikeC4VitePlugin({})],
+  },
+  integrations: [starlight({
+    title: {
+      hu: "Szoftverrendszerek tervezése",
+      en: "Advanced Software Engineering",
+    },
+    customCss: ["./src/styles/custom.css"],
+    defaultLocale: "hu",
+    locales: {
+      en: {
+        label: "English",
       },
-      customCss: ["./src/styles/custom.css"],
-      defaultLocale: "hu",
-      locales: {
-        en: {
-          label: "English",
-        },
-        hu: {
-          label: "Magyar",
-        },
+      hu: {
+        label: "Magyar",
       },
-      social: [
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/unideb-advanced-software-engineering",
+    },
+    social: [
+      {
+        icon: "github",
+        label: "GitHub",
+        href: "https://github.com/unideb-advanced-software-engineering",
+      },
+    ],
+    sidebar: [
+      {
+        label: "Általános",
+        translations: {
+          en: "General",
         },
-      ],
-      sidebar: [
-        {
-          label: "Általános",
-          translations: {
-            en: "General",
-          },
-          autogenerate: { directory: "general" },
+        autogenerate: { directory: "general" },
+      },
+      {
+        label: "Nappali gyakorlatok",
+        translations: {
+          en: "Classes",
         },
-        {
-          label: "Nappali gyakorlatok",
-          translations: {
-            en: "Classes",
-          },
-          autogenerate: { directory: "classes" },
+        autogenerate: { directory: "classes" },
+      },
+      {
+        label: "Esettanulmányok",
+        translations: {
+          en: "Scenarios",
         },
-        {
-          label: "Esettanulmányok",
-          translations: {
-            en: "Scenarios",
-          },
-          collapsed: true,
-          autogenerate: { directory: "scenarios" },
-        },
-      ],
-    }),
-    markdoc(),
-  ],
+        collapsed: true,
+        autogenerate: { directory: "scenarios" },
+      },
+    ],
+  }), markdoc(), react()],
 });
--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -1,6 +1,16 @@
-import { defineMarkdocConfig } from "@astrojs/markdoc/config";
+import { component, defineMarkdocConfig } from "@astrojs/markdoc/config";
 import starlightMarkdoc from "@astrojs/starlight-markdoc";
 
 export default defineMarkdocConfig({
   extends: [starlightMarkdoc()],
+  tags: {
+    likec4: {
+      render: component("./src/components/LikeC4View.astro"),
+      attributes: {
+        viewId: {
+          type: String,
+        },
+      },
+    },
+  },
 });
